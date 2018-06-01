f37a0aeb42bc0a1b0bf7818a6574f127a9e4e54c
Kohei.Saito <Kohei.Saito@sony.com>
2018-06-01 18:34:49 +0900

    create tutorial examples

--- src/main.js
+++ src/main.js
@@ -1,8 +1,83 @@
 import Vue from 'vue'
 
-new Vue({
+var app = new Vue({
   el: '#app',
+  data: {
+    message: 'Hello Vue!!!!'
+  }
+})
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awseome' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
   data: {
     message: 'Hello Vue!'
   }
 })
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+
+var watchFunc = function (newValue, oldValue) {
+  return newValue.split('').reverse().join('')
+}
+var app8_data = {
+  value: 'Hello !'
+}
+var app8 = new Vue({
+  el: '#app-8',
+  data: app8_data,
+  computed: {
+    reversedMessage: function () { return watchFunc(app8_data.value, app8_data.value) }
+  }
+})
+
